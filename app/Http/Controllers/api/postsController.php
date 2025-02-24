2145165f5c4ca9aeffb830f9982bdc844a432778
Ibrahem-Khaled <xx.vvv48@gmail.com>
2025-02-25 01:59:56 +0200

    add can create post

--- app/Http/Controllers/api/postsController.php
+++ app/Http/Controllers/api/postsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class postsController extends Controller
 {
@@ -16,5 +17,34 @@ class postsController extends Controller
             ->get();
 
         return response()->json($posts);
+    }
+
+    public function create(Request $request)
+    {
+        $user = auth()->guard('api')->user();
+
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'content' => 'nullable|string',
+            'status' => 'required|in:active,inactive,banned',
+            'images.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        $data = $request->except('images');
+
+        // معالجة الصور
+        if ($request->hasFile('images')) {
+            $images = [];
+            foreach ($request->file('images') as $image) {
+                $path = $image->store('public/posts');
+                $images[] = str_replace('public/', '', $path);
+            }
+            $data['images'] = json_encode($images);
+        }
+        $post = Post::create($data);
+        return response()->json($post);
     }
 }
