0fedeb94d9610a762867e3d808d4f8a49d03400e
Ibrahem-Khaled <xx.vvv48@gmail.com>
2025-03-28 07:53:39 +0200

    fix bug from column name

--- app/Traits/UserAccessors.php
+++ app/Traits/UserAccessors.php
@@ -42,6 +42,8 @@ trait UserAccessors
 
     public function getIsAuthanticatedUserBlockedThisUserAttribute()
     {
-        return $this->blockedUsers()->where('blocked_user_id', auth()->guard('api')->id())->exists();
+        return $this->blockedUsers()->where('blocked_user_id', auth()->guard('api')->id())
+            ->orWhere('user_id', auth()->guard('api')->id())
+            ->exists();
     }
 }
