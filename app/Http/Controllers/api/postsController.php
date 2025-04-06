46d1c8631c2675644e4f31a656feff11160721b2
Ibrahem-Khaled <xx.vvv48@gmail.com>
2025-04-06 09:26:26 +0200

    fix bug from block users api

--- app/Http/Controllers/api/postsController.php
+++ app/Http/Controllers/api/postsController.php
@@ -91,7 +91,7 @@ class postsController extends Controller
 
             $post->update($data);
 
-            return response()->json($post);
+            return response()->json('تم تحديث المنشور بنجاح');
         }
         return response()->json(['message' => 'غير مصرح به'], 401);
     }
