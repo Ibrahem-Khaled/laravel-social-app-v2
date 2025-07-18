6ffbf4339220989de871ca6d64fd4687b4e978f6
ibrahem khaled <xx.vvv48@gmail.com>
2025-07-19 01:51:55 +0300

    fix and improve code

--- app/Http/Controllers/api/postsController.php
+++ app/Http/Controllers/api/postsController.php
@@ -9,6 +9,10 @@ use App\Services\NotificationService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
+use Throwable;
+
 
 class postsController extends Controller
 {
