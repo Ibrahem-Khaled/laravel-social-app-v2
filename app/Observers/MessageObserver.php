6fa004bc753e23d22913333433699d8d9d523904
ibrahem khaled <xx.vvv48@gmail.com>
2025-07-28 08:03:39 +0300

    change chat to real time

--- app/Observers/MessageObserver.php
+++ app/Observers/MessageObserver.php
@@ -4,14 +4,16 @@ namespace App\Observers;
 
 use App\Models\Message;
 use App\Events\Chat;
+use Illuminate\Support\Facades\Log; //  ✨ أضف هذا السطر
 
 class MessageObserver
 {
     /**
      * Handle the Message "created" event.
      */
-     public function created(Message $message): void
+    public function created(Message $message): void
     {
+        Log::info("MessageObserver 'created' method triggered for message ID: {$message->id}");
         broadcast(new Chat('created', $message));
     }
 
